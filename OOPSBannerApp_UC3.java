cf974c9b1f862135e445c0da1fb33001edac0c19
tarun-mishra-1208 <tmishra1208@gmail.com>
2026-03-31 17:12:38 +0530

    OOPSBANNERAPP

---
+++ OOPSBannerApp_UC3.java
@@ -0,0 +1,13 @@
+public class OOPSBannerApp_UC3 {
+    public static void main(String[] args) {
+        System.out.println(String.join(" ", "    ***    ", "    ***    ", " *******    ", "    *****  "));
+        System.out.println(String.join(" ", "  **   **  ", "  **   **  ", " **     **  ", "  **      "));
+        System.out.println(String.join(" ", " **     ** ", " **     ** ", " **      ** ", " **     "));
+        System.out.println(String.join(" ", " **     ** ", " **     ** ", " **     **  ", "  **   "));
+        System.out.println(String.join(" ", " **     ** ", " **     ** ", " *******    ", "    ***   "));
+        System.out.println(String.join(" ", " **     ** ", " **     ** ", " **         ", "       **"));
+        System.out.println(String.join(" ", " **     ** ", " **     ** ", " **         ", "        **"));
+        System.out.println(String.join(" ", "  **   **  ", "  **   **  ", " **         ", "       **"));
+        System.out.println(String.join(" ", "    ***    ", "    ***    ", " **         ", "  *****    "));   
+}
+}
